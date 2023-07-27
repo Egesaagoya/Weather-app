45fcdddd174be08ae27dc3da9c35439995def34f
Egesaagoya <egesaagoya.com>
2023-07-27 04:23:12 +0300

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
 <template>
   <div class="app">
     <div class="header container h-100 p-5">
-      <h1 class mb-5>Weather App</h1>
+      <h1 class="mb-5">Weather App</h1>
       <div class="d-flex justify-content-center h-100">
         <div class="searchbar w-50 mx-2">
-          <input type="text" class="input form-control" placeholder="Enter City/Town">
+          <input type="text" class="input form-control" v-model="city" placeholder="Enter City/Town">
         </div>
         <button class="btn-search btn btn-primary">Search <i class="fas fa-search"></i></button>
       </div>
     </div>
+    <br>
+    <weather></weather>
   </div>
 </template>
 
 <script>
+
+import weather from './components/weather.vue';
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    weather
+  },
+  data() {
+    return {
+      city:"",
+      showWeather:false,
+    }
+  },
+  methods: {
+    async searchWeather(){
+      this.showWeather=false;
+      await this.$nextTick();
+      this.showWeather=true;
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+body{
+  background-color: #0e0e0f;
+}
+.header{
+  background-color:#212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  margin-top: 5rem;
+}
+.btn-search{
+  background-image: linear-gradient(to right, cyan ,magenta);
+}
+</style>
---
+++ src/components/weather.vue
@@ -0,0 +1,148 @@
+<template>
+   <div class="container p-0">
+    <div class="d-flex">
+        <div class="card main-div w-100">
+            <div class="p-3">
+                <div class="p-3">
+                    <h3 class="mb-1 day">Monday</h3>
+                    <p class="text-light date mb-0">Date</p>
+                    <small>Time</small>
+                    <h2 class="place"><i class="fas fas location">Mombasa<small>Country</small></i></h2>
+                    <div class="temp">
+                        <h1 class="weather-temp">20&deg;</h1>
+                        <h2 class="text-light">Description</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card card-2 w-100">
+        <table class="m-4">
+            <tbody>
+                <tr>
+                    <th>Sea Level</th>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <th>Sea Level</th>
+                    <td>100</td>
+                </tr> 
+                <tr>
+                    <th>Sea Level</th>
+                    <td>100</td>
+                </tr>
+            </tbody>
+        </table>
+        <div id="div_Form" class="d-flex m-3 justify-content-center">
+            <form action="">
+                <input type="vutton" value="Change Location" class="btn change-btn btn-primary">
+            </form>
+        </div>
+    </div>
+   </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "myWeather",
+    components: {},
+    data() {
+      return {
+        city:"",
+        showWeather:false,
+      }
+    },
+    methods: {
+      async searchWeather(){
+        this.showWeather=false;
+        await this.$nextTick();
+        this.showWeather=true;
+      }
+    },
+  };
+  </script>
+  
+  <style>
+ body{
+    background-color: #343d4b;
+ }
+ .weather-temp{
+    margin: 0%;
+    font-weight: 700;
+    font-size: 4em;
+ }
+ h2.mb-1.day{
+    font-size: 3rem;
+    font-weight: 400;
+ }
+ .main-div{
+    border-radius: 20px;
+    color: #fff;
+    background-image: url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fweather%2F&psig=AOvVaw0ULgVWKr3HlEOaxVH2AacV&ust=1690506773352000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICFsc_arYADFQAAAAAdAAAAABAE);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: overlay;
+    background-color: rgba(0, 0, 0, 0.5);
+    background-repeat: no-repeat;
+ }
+ .temp{
+    position: absolute;
+    bottom: 0;
+ }
+ .card-2{
+    background-color: #212730;
+    border-radius: 20px;
+ }
+ .main-div:hover{
+    transform: scale(1.1);
+    transition: transform 0.5s ease;
+    z-index: 1;
+ }
+ .card-details{
+    margin-left: 19px;
+ }
+.h1_left{
+    position: absolute;
+    bottom: 25px;
+    left: 16px;
+    font-size: 3vw;
+    line-height: 1.2;
+}
+h3_left{
+    position: absolute;
+    left: 16px;
+    font-size: 2vw;
+    line-height: 0.5;
+}
+.h3_left small{
+    font-size: 1rem;
+}
+table{
+    position: relative;
+    left: 16px;
+    border-collapse: separate;
+    border-spacing: 15px;
+    width: 85%;
+    text-align: left;
+    max-width: 600px;
+    margin: 0 auto;
+
+}
+th,
+td{
+    font-size: 18px;
+    color: #fff;
+}
+ td{
+    text-align: right;
+ }
+ table,tr:hover{
+    color: red;
+ }
+ .change-btn{
+    background-image: linear-gradient(to right, cyan ,magenta );
+ }
+
+
+  </style>
+  
